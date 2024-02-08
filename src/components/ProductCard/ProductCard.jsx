1a5aa0da78d75d6188422d6999f035a2656446d9
Dhenzel Lima Ribeiro <dhenzellimaribeiro@gmail.com>
2024-02-08 10:39:59 -0300

    FEAT: Product on modal

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -31,6 +31,20 @@ function ProductCard({ data }) {
       {showModal && (
         <div className="modal">
           <div className="modal-content">
+            <div className="modal-item">
+              <div className="cart-item">
+                <img
+                  src={thumbnail}
+                  alt="imagem do produto"
+                  className="cart-item-image"
+                />
+
+                <div className="cart-item-content">
+                  <h3 className="cart-item-title">{title}</h3>
+                  <h3 className="cart-item-price">{formatCurrency(price, 'BRL')}</h3>
+                </div>
+              </div>
+            </div>
             <p>Deseja adicionar este item ao carrinho?</p>
             <div>
               <button type="button" onClick={handleConfirmAdd}>Sim</button>
