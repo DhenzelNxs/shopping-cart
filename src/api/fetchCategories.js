331f3af53f178234a4e6295ed4a09b20eb74afed
Dhenzel Lima Ribeiro <dhenzellimaribeiro@gmail.com>
2024-02-16 20:37:25 -0300

    FIX: conection not found

--- src/api/fetchCategories.js
+++ src/api/fetchCategories.js
@@ -1,8 +1,14 @@
 const fetchCategories = async () => {
-  const response = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
-  const data = await response.json();
 
-  return data;
+  try {
+    const response = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    return error;
+  }
+
 };
 
 export default fetchCategories;
--- src/api/fetchProducts.js
+++ src/api/fetchProducts.js
@@ -1,8 +1,14 @@
 const fetchProducts = async (query) => {
-  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
-  const data = await response.json();
 
-  return data.results;
+  try {
+    const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
+    const data = await response.json();
+
+    return data.results;
+  } catch (error) {
+    return error;
+  }
+  
 };
 
 export default fetchProducts;
