218f2a652dc1d94a94127ae37bb81f2ae65adfda
Dhenzel Lima Ribeiro <dhenzellimaribeiro@gmail.com>
2024-02-16 21:02:14 -0300

    FEAT: Screens Tokens Generator

--- src/context/Provider.jsx
+++ src/context/Provider.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import propTypes from 'prop-types';
 import AppContext from './AppContext';
 import { useNavigate } from 'react-router-dom';
+import Tokens from '../utils/ScreenTokensGenerator';
 
 function Provider({ children }) {
 
@@ -12,9 +13,9 @@ function Provider({ children }) {
   const [isCartVisible, setIsCartVisible] = useState(false);
   const navigate = useNavigate();
   const ScreenTokens = {
-    Order: 'IDJIPJAINOV9828',
-    Payment: 'APOJAINFIBANO8',
-    QrCode: 'JHSUHF79293HIIFH',
+    Order: Tokens.Order,
+    Payment: Tokens.Payment,
+    QrCode: Tokens.QrCode,
   };
 
   const value = {
---
+++ src/utils/ScreenTokensGenerator.js
@@ -0,0 +1,16 @@
+const GerarToken = () => {
+  const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let token = '';
+  for (let i = 0; i < 9; i++) {
+    token += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
+  }
+  return token;
+};
+
+const Tokens = {
+  Order: GerarToken(),
+  Payment: GerarToken(),
+  QrCode: GerarToken(),
+};
+
+export default Tokens;
